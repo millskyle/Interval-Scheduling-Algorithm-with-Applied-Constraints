e8103f875861c466a0ddca14199af6deaa895fa7
Kyle Mills <kyle.mills@uoit.net>
2015-08-02 20:45:08 -0400

    moved graph building stuff to build_graph.py

--- src/build_graph.py
+++ src/build_graph.py
@@ -1,6 +1,6 @@
 import matplotlib.pyplot as plt
 import networkx as nx
-import json
+from JSON_io import JSON_write
 from course import *
 
 
@@ -8,81 +8,81 @@ from course import *
 #        a list of all potential courses (as Section() objects), returned 
 #        from some database query or web scraping action.
 
+#  populate this list with Section() objects from the query
 query_results = []
+
+#  populate this with the total number of sections that should be on
+#  the calendar, assuming there are no scheduling impossibilities 
+#  (e.g.  1 tutorial, 1 lab and 1 lecture each for 3 courses would 
+#  result in requiredNumberOfSections = 9)
+requiredNumberOfSections = 10
+
+
+
+##start code
+
 
 
 #Construct the graph object
 G = nx.Graph()
-
 
 #add all potential courses as nodes to the graph
 for Sec in query_results:
    G.add_node(Sec, label=Sec.course[0:2])
 
 
-edges = []
-val_map = { 'Lec': 1.0, 'Tut':0.75, 'Lab':0.25, 'Oth':0.0 }
-colors = [val_map[node.cType] for node in G.nodes() ]
+#map the type to a float for coloring the graph output
+# {
+typemapping = { 'Lec': 1.0, 'Tut':0.75, 'Lab':0.25, 'Oth':0.0 }
+colors = [typemapping[node.cType] for node in G.nodes() ]
+# }
 
-
-for i,iSec in enumerate(allcourses):
-   for j,jSec in enumerate(allcourses):
+for i,iSec in enumerate(query_results):
+   for j,jSec in enumerate(query_results):
       if i<j:
          have_edge = False
-         #If the two sections are from the same course and are the same type, they're incompatible
          if ( (iSec.cType == jSec.cType) and (jSec.course==iSec.course)):
+            #If the two sections are from the same course and are the same type, then they're incompatible.
+            #draw an edge between them
             have_edge = True
          for itimeslot in iSec.timeslots:
             for jtimeslot in jSec.timeslots:
-               if ( (jtimeslot.sTime <= itimeslot.eTime and jtimeslot.eTime >= itimeslot.eTime and jtimeslot.day==itimeslot.day) ):
+               if ((jtimeslot.sTime <= itimeslot.eTime and jtimeslot.eTime >= itimeslot.eTime and jtimeslot.day==itimeslot.day)):
+                  #if they overlap in time, and they're on the same day, then they're incompatible.
                   have_edge = True
 
          if have_edge:
+            #If have_edge is true, then these two nodes are incompatible with each other.
+            #Add an edge between them
             G.add_edge(iSec,jSec)
-            edges.append([i,j])
 
-print "Your schedule: "
-requiredNumberOfSections = 10
-x = 0
+
+
+
+
+successfully_scheduled_sections = 0
 tries = 0
-while x < requiredNumberOfSections:
+
+
+while successfully_scheduled_sections < requiredNumberOfSections:
+   #compute the maximal independent set.  This is NOT the MAXIMUM independent set. Thus we must loop a few times
+   #to attempt to get the best possible optimization.
    yoursched = nx.maximal_independent_set(G)
-   x =  len(yoursched)
+   successfully_scheduled_sections =  len(yoursched)
    tries +=1
    print "Attempt ",tries
-   if tries > 1000:
+   if tries > 100:
       print "Unsuccessful at making a schedule which includes all courses.  The best one possible has been created, but please check for missing courses/sections"
       break
 
 
-w1jsonData = []
-w2jsonData = []
-for CC in yoursched:
-   print "  "
-   w1js,w2js = CC.toDict()
-   for w1jsd in w1js:
-      w1jsonData.append(w1jsd)
-   for w2jsd in w2js:
-      w2jsonData.append(w2jsd)
-
-   print CC.course, CC.cType, CC.CRN
-   for ts in CC.timeslots:
-      print "   ",ts.day, ts.sTime, ts.eTime
 
 nx.draw_networkx(G,with_labels=True,labels=nx.get_node_attributes(G,'unique'), cmap=plt.get_cmap('jet'), node_color=colors,vmin=0, vmax=1.0)
 plt.savefig('graph.svg')
 
 
 
-
-with open('w1.json','w') as outfile:
-  json.dump(w1jsonData, outfile, sort_keys = True, indent = 4, ensure_ascii = True)
-
-with open('w2.json','w') as outfile:
-  json.dump(w2jsonData, outfile, sort_keys = True, indent = 4, ensure_ascii = True)
-
-
-print "done"
+JSON_write(yoursched, 'w1.json','w2.json')
 
 
 
