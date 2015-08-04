48866360c93c0e71a20f90c47dab73d1f35772ac
nbuhagiar <nicholas.buhagiar@uoit.net>
2015-08-04 17:14:42 -0400

    Continued work on web form input page

--- src/webserver/static/input.js
+++ src/webserver/static/input.js
@@ -1,6 +1,6 @@
-function loadXMLDoc() {
+var xmlhttp;
 
-	var xmlhttp;
+function loadXMLDoc() {
 
 	if (window.XMLHttpRequest) {
 		xmlhttp=new XMLHttpRequest();
@@ -11,27 +11,40 @@ function loadXMLDoc() {
 
 	xmlhttp.onreadystatechange=function() {
   		if (xmlhttp.readyState==4 && xmlhttp.status==200) {
-    		document.getElementById("myDiv").innerHTML=xmlhttp.responseText;
+    		alert(xmlhttp.responseText);
     	}
   	}
 
-xmlhttp.open("POST",".asp",true);
+xmlhttp.open("GET","/getAvailableCourses",true);
 xmlhttp.send();
-
 }
 
 $(function () {
 	
 	$("#b1").click(function() {
 		$("#l1 option:selected").each(function(){
-			$(this).remove().appendTo("l2");
+			$(this).remove().appendTo("#l2");
 		});
 	});
 
 	$("#b2").click(function() {
 		$("#l2 option:selected").each(function(){
-			$(this).remove().appendTo("l1");
+			$(this).remove().appendTo("#l1");
 		});
-	});	
+	});
+
+	$("#b3").click(function() {
+		$("#l3 option:selected").each(function(){
+			$(this).remove().appendTo("#l4");
+		});
+	});
+
+	$("#b4").click(function() {
+		$("#l4 option:selected").each(function(){
+			$(this).remove().appendTo("#l3");
+		});
+	});		
 
 });
+
+loadXMLDoc();
