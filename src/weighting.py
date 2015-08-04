7a8c7cac65d3a4781f11260e78b8de8e909a8b76
Kyle Mills <kyle.mills@uoit.net>
2015-08-04 15:44:08 -0400

    fixed naming, typos

--- src/weighting.py
+++ src/weighting.py
@@ -2,21 +2,20 @@ from course import *
 
 
 
-def myWeighting(Schedule):
+def myScore(Schedule):
    weight = 0.
    for CRN in Schedule:
-       for ts in CRN.timesteps:
-       #   doSomeMeth
+       for ts in CRN.timeslots:
+       #   doSomeMath
        junk=1
    return 0 # return a float from -1 to 1
 
 
 
-
 def compute_schedule_weight(Schedule):
-   weight = 0.0
-   weight += myWeighting(Schedule)
-   return weight
+   score = 0.0
+   score += myScore(Schedule)
+   return score
 
 
 
