f47e23b3cac747c47de27dd87fe0d45a8f64a48f
kkastr <dinokastritis@gmail.com>
2015-08-05 16:00:00 -0400

    added a rough check for same day

--- src/dinoWeighting.py
+++ src/dinoWeighting.py
@@ -2,7 +2,7 @@ from course import *
 
 
 
-
+"""
 def course_density(Schedule):
    weight = 0.
    counter = 0.
@@ -14,9 +14,9 @@ def course_density(Schedule):
                         counter+=1.0
                         weight+=1.0
    return weight/counter# return a float from -1 to 1
-
-
 """
+
+
 def course_density(Schedule):
     weight = 0.
     counter = 0.
@@ -24,14 +24,18 @@ def course_density(Schedule):
         for jCRN in Schedule:
             for i in iCRN.timeslots:
                 for j in jCRN.timeslots:
-                    mints=min(i.sTime)
-                    maxts=max(j.eTime)
-                    time = int(j.eTime) - int(i.sTime)
-                    if time>=10:
-                        weight+=1.
-                        counter+=1.
+                    #mints=min(i.sTime)
+                    #maxts=max(j.eTime)
+                    if i.day==j.day: 
+                        time = int(j.eTime) - int(i.sTime)
+                        if time<=40:
+                            weight+=1.
+                            counter+=1.
     return weight/counter
-"""
+
+
+
+
 
 def compute_schedule_score(Schedule):
    score = 0.0
