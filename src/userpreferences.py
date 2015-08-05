f81e138a76b0e7e477af865953e508acd761e79e
Kyle Mills <kyle.mills@uoit.net>
2015-08-05 12:49:06 -0400

    userpreferences.py

--- src/userpreferences.py
+++ src/userpreferences.py
@@ -6,7 +6,7 @@ class UserPreferences():
    def __init__(self):
       self.semester = "201509"
       self.MaximizeDaysOff = False
-      self.PreferredDaysOff = [5,]
+      self.PreferredDaysOff = [1,2,3,4,5]
 
 
 UserPrefs = UserPreferences()
