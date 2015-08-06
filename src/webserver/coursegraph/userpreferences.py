d573293998fa790ec980a99363d20aa448a7e2d7
nbuhagiar <nicholas.buhagiar@uoit.net>
2015-08-06 10:07:52 -0400

    Updated userpreferences file

--- src/webserver/coursegraph/userpreferences.py
+++ src/webserver/coursegraph/userpreferences.py
@@ -5,8 +5,14 @@
 class UserPreferences():
    def __init__(self):
       self.semester = "201509"
-      self.MaximizeDaysOff = False
-      self.PreferredDaysOff = [5,]
-
-
+      self.MaximizeDaysOff = True
+      self.PreferredDaysOff = [1,2,3,4,5,6,7,8,9,10]
+      self.PreferTimeOfDay = "Evening" 
+      # Options "Morning", "Afternoon", "Evening"
+      self.optimumTimeOfDay = "0800"
+      # Sets preferred time for Gaussian
+      self.preferredCampus = 0
+      #0 for none, 1 for north, 2 for dt
+      self.preferOnline = False
+      self.preferredCRNs = []
 UserPrefs = UserPreferences()
