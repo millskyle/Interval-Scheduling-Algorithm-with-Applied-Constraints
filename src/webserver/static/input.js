bd290a300c37d20f3f63be7072829325d142d647
David Nemirovsky <david@nemirovd.com>
2015-08-06 21:46:28 -0400

    Javascript Fix

--- src/webserver/static/input.js
+++ src/webserver/static/input.js
@@ -2,17 +2,18 @@ var xmlhttp;
 var courses;
 
 function loadXMLDoc() {
+	$('#l1').empty()
+	$('#l2').empty()
+	$('#l3').empty()
+	$('#l4').empty()
+	$('#l5').empty()
+	$('#l6').empty()
 	var sem = $('#semester').find(":selected").text();
 	$.ajax({
 		type: 'get',
 		url: '/getAvailableCourses/'+sem,
 		success: function(data){
 			courses = JSON.parse(data);
-			if(!courses[sem]) {
-				$('#l1').empty()
-				$('#l4').empty()
-				return;
-			}
 			$.each(courses[sem], function(key, value) {   
   			    $('#l1').append($("<option></option>").attr("value",key).text(key)); 
   			});
