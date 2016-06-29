2d9b16dc497c649d0d3cdffcc3ce6334747ae565
Kyle Mills <kyle.mills@uoit.net>
2016-06-29 11:43:53 -0400

    added loop to crawler.sh

--- src/webserver/crawl.sh
+++ src/webserver/crawl.sh
@@ -4,4 +4,10 @@ cd $HOME/Interval-Scheduling-Algorithm-with-Applied-Constraints/src/webserver
 
 # If you want to use a different python interpreter, set your
 # $PATH to something else.
-python crawl.py
+for i in {1..10};
+do
+  python crawl.py
+  sleep(600)
+done
+
+
