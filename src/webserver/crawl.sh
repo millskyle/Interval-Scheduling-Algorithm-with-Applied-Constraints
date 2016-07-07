2ab67e399a209ff9f81c617deec7d4ea64ee82ef
Kyle Mills <kyle.mills@uoit.net>
2016-07-07 11:33:39 -0400

    removed crawl loop

--- src/webserver/crawl.sh
+++ src/webserver/crawl.sh
@@ -4,10 +4,10 @@ cd $HOME/Interval-Scheduling-Algorithm-with-Applied-Constraints/src/webserver
 
 # If you want to use a different python interpreter, set your
 # $PATH to something else.
-for i in {1..8};
-do
+#for i in {1..8};
+#do
   python crawl.py
-  sleep 60
-done
+#  sleep 60
+#done
 
 
